7ccde53bd58abf11892abb81edfe6f442454a8cd
AAIZAHMED <aaiza2@umbc.edu>
2017-01-13 15:20:30 -0500

    do it again

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -34,7 +34,7 @@ public class BinarySearchTree < T extends Comparable<? super T> >
 
 	private BinaryNode<T> root;
 	
-	public BinarySearchTree(T data)
+	public BinarySearchTree ()
 	{	root = null;	}
 
 	public boolean isEmpty()
@@ -43,135 +43,7 @@ public class BinarySearchTree < T extends Comparable<? super T> >
 	public void makeEmpty()
 	{	root = null;	}
 
-	public boolean contains (T data)
-	{
-		return contains(data, root);
-	}
-
-	private boolean contains (T data, BinaryNode<T> node)
-	{
-		if (node == null)
-		{	return false;	}
-
-		int compareResult = data.compareTo(node.data);
-
-		if (compareResult < 0)
-		{
-			return contains(data, node.leftNode);
-		}
-		else if (compareResult > 0)
-		{
-			return contains(data, node.rightNode);
-		}
-		else return true;	//Match
-	}
-
-	public void insert (T data)
-	{	root = insert(data, root);	}
-
-	private BinaryNode<T> insert(T data, BinaryNode<T> root)
-	{
-		if (root == null)
-		{
-			root = new BinaryNode<T> (data);
-		}
-
-		int compareResult = data.compareTo(root.data);
-
-		if (compareResult < 0)
-		{
-			return insert(data, root.leftNode);
-		}
-		else if (compareResult > 0)
-		{
-			return insert(data, root.rightNode);
-		}
-		else // Duplicate, do nothing
-		{			}
-
-		return root;
-	}
-
-	public T findMin ()
-	{
-		if( isEmpty() ) 
-		{
-			throw new RuntimeException( "Tree is empty." );
-		}
-		return findMin(root).data;
-	}
-
-	private BinaryNode<T> findMin (BinaryNode<T> node)
-	{
-		if (node == null) 
-		{
-			return null;	
-		}
-		else if (node.leftNode == null) 
-		{
-			return node;	
-		}
-
-		return findMin (node.leftNode);
-	}
-
-	public T findMax ()
-	{
-		if( isEmpty() ) 
-		{
-			throw new RuntimeException( "Tree is empty." );
-		}
-		return findMax(root).data;
-	}
-
-	private BinaryNode<T> findMax (BinaryNode<T> node)
-	{
-		if (node == null) 
-		{
-			return null;	
-		}
-		else if (node.rightNode == null) 
-		{
-			return node;	
-		}
-		
-		return findMax (node.rightNode);
-	}
-
-	public void remove(T data)
-	{
-		root = remove (data, root);
-	}
-
-	private BinaryNode<T> remove (T data, BinaryNode<T> node)
-	{
-		if (root == null)
-		{
-			return null;
-		}
-
-		int compareResult = data.compareTo(root.data);
-
-		if (compareResult < 0)
-		{
-			return remove(data, root.leftNode);
-		}
-		else if (compareResult > 0)
-		{
-			return remove(data, root.rightNode);
-		}
-		else if (node.leftNode != null && node.rightNode != null) 
-		{
-			node.data = findMin(node).data;
-			node.rightNode = remove (node.data, node.rightNode);
-		}
-		else
-		{	//Using using ternary operator, syntax==> X = (condition) ? if true do this : else do this;
-			node = (node.leftNode != null) ? node.leftNode : node.rightNode;
-		}
-
-		return node;
-	}
+	//Add here
 	
 	public void printTree () 
 	{
@@ -182,15 +54,35 @@ public class BinarySearchTree < T extends Comparable<? super T> >
 		else printTree (root);
 	}
 
-	private void printTree(BinaryNode<T> root) 
-	{
-		
-		if (root != null)	//In-order traversal.
+	private void printTree(BinaryNode<T> node) 
+	{		
+		if (node != null)	//In-order traversal.
 		{
-			printTree (root.leftNode);
-			System.out.println(root.data);
-			printTree (root.rightNode);
+			printTree (node.leftNode);
+			System.out.println(node.data);
+			printTree (node.rightNode);
 		}		
 	}
+	
+	/** 
+	 * This method returns how many nodes/values are in this tree
+	 * @return the number of nodes in this tree
+	 */
+	public int size() 
+	{	return size(root);	}
+	
+	/**
+	 * Calculates the size of the tree.
+	 * @param x
+	 * @return size
+	 */
+	private int size(BinaryNode<T> node) 
+	{
+		if (node == null) 
+		{
+			return 0;             
+		} 
+		return size(node.leftNode) + 1 + size(node.rightNode);
+	}	
 
 }
