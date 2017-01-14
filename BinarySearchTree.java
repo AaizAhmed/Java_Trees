ab7d1fd1fcebd4618b0a87a9d320e9340031f9e4
Aaiz Ahmed <aaiza2@umbc.edu>
2017-01-14 14:13:50 -0500

    rewritten

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,4 +1,4 @@
-package Java_Trees;
+package trees;
 
 /**
  * A Generic Binary Search Tree class.
@@ -43,7 +43,147 @@ public class BinarySearchTree < T extends Comparable<? super T> >
 	public void makeEmpty()
 	{	root = null;	}
 
-	//Add here
+	public boolean contains (T data)
+	{
+		return contains(data, root);
+	}
+	
+	private boolean contains (T data, BinaryNode<T> node)
+	{		
+		if (node == null)
+		{	return false;	}
+		
+		int compareResult = data.compareTo(node.data);
+		
+		if (compareResult < 0)
+		{
+			return contains(data, node.leftNode);
+		}
+		else if (compareResult > 0)
+		{
+			return contains(data, node.rightNode);
+		}
+		else     //Match			
+			return true;
+	}
+	
+	public void insert(T data) 
+	{
+		root = insert (data, root);
+	}
+	
+	private BinaryNode<T> insert (T data, BinaryNode<T> node)
+	{
+		if (node == null)
+		{
+			return new BinaryNode<T> (data);
+		}
+		
+		int compareResult = data.compareTo(node.data);
+		
+		if (compareResult < 0)
+		{
+			node.leftNode = insert (data, node.leftNode);
+		}
+		else if (compareResult > 0)
+		{
+			node.rightNode = insert (data, node.rightNode);
+		}
+		else    // Duplicate
+		{    
+			
+		}
+		
+		return node;		
+	}
+	
+	public T findMin() 
+	{
+		if ( isEmpty () )
+		{
+			throw new RuntimeException("Tree is empty.");
+		}
+		
+		return findMin (root).data;
+	}
+	
+	private BinaryNode<T> findMin (BinaryNode<T> node)
+	{
+		if (node == null)
+		{
+			return null;
+		}
+		
+		if (node.leftNode == null)
+		{
+			return node;
+		}
+		
+		return findMin (node.leftNode);
+	}
+	
+	public T findMax()
+	{
+		if ( isEmpty () )
+		{
+			throw new RuntimeException("Tree is empty.");
+		}
+		
+		return findMax(root).data;
+	}
+	
+	private BinaryNode<T> findMax (BinaryNode<T> node)
+	{
+		if (node == null)
+		{
+			return null;
+		}
+		
+		if (node.rightNode == null)
+		{
+			return node;
+		}
+		
+		return node.rightNode;
+	}
+	
+	public void remove (T data)
+	{
+		if ( isEmpty() )
+		{
+			throw new RuntimeException("Tree is empty.");
+		}
+		
+		root = remove (data, root);
+	}
+	
+	private BinaryNode<T> remove (T data, BinaryNode<T> node)
+	{
+		if (node == null)
+		{
+			return null;
+		}
+		
+		int compareResult = data.compareTo(node.data);
+		
+		if (compareResult < 0)
+		{
+			node.leftNode = remove (data, node.leftNode);
+		}
+		else if (compareResult > 0)
+		{
+			node.rightNode = remove (data, node.rightNode);
+		}
+		else if (node.leftNode != null && node.rightNode != null) //Two children
+		{
+			node.data = findMin(node).data;  //removing the data and writing the new value
+			node.rightNode = remove (node.data, node.rightNode);
+		}
+		
+		node = (node.leftNode != null) ? node.leftNode : node.rightNode;
+		
+		return node;
+	}
 	
 	public void printTree () 
 	{
